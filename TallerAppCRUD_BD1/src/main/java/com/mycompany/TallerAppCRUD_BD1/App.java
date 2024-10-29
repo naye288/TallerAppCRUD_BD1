fc123c79743afaa7272380cf9229b22457a51b3e
Sebas3004 <sebasmumor2630@gmail.com>
2024-10-29 16:07:55 -0600

    Proyecto

---
+++ TallerAppCRUD_BD1/src/main/java/com/mycompany/TallerAppCRUD_BD1/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.TallerAppCRUD_BD1;
+
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Pantalla pantalla = new Pantalla();
+        Pantalla.crearPantalla(pantalla);
+    }
+
+}
